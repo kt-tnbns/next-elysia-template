7a406c66003e34691f18beb05cb18b9f07a0648c
kt-tnbns <tangg6@outlook.com>
2025-11-11 21:14:13 +0700

    fix: path of prisma

--- core/src/infrastructure/database/prisma.client.ts
+++ core/src/infrastructure/database/prisma.client.ts
@@ -1,4 +1,4 @@
-import { PrismaClient } from '../../generated/prisma/client';
+import { PrismaClient } from './prisma/generated/prisma/client';
 
 // Prisma Client singleton pattern for Bun
 // Prevents multiple instances in development with hot-reload
